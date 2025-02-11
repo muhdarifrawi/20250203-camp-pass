ede57e220072c11730d31e69faa67d76dfb75dea
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2025-02-11 14:48:44 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,11 @@
+function mockInfo() {
+    let data = {
+        "personnel": [
+            { "firstName": "John", "lastName": "Doe", "username":"johndoe", "password":"password123" },
+            { "firstName": "Jack", "lastName": "Joe", "username":"jackjoe", "password":"password123" },
+            { "firstName": "Peter", "lastName": "Jackson", "username":"peterjackson", "password":"password123" }
+        ]
+    }
+
+    return data
+}
---
+++ login.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM Loaded");
+    loadLogin();
+})
+
+function loadLogin(){
+    let loginBtn = document.querySelector("#login-btn");
+    let adminLoginBtn = document.querySelector("#admin-login-btn");
+
+    if(loginBtn){
+        loginBtn.addEventListener("click", () => {handleLogin(event)});
+    }
+    if(adminLoginBtn){
+        adminLoginBtn.addEventListener("click", () => {handleLogin(event)});
+    }
+}
+
+function handleLogin(event){
+    console.log("LOGIN")
+    event.preventDefault();
+    let errorMsg = document.querySelector("#error-msg");
+    errorMsg.innerHTML = "";
+    let username = document.querySelector("#usernameInput").value;
+    let password = document.querySelector("#passwordInput").value;
+    console.log(username,password);
+
+    let data = mockInfo();
+    
+    let personnelData = data.personnel;
+    let users = personnelData.map((p)=> {
+        return p.username
+    });
+    let passwords = personnelData.map((p)=> {
+        return p.password
+    });
+
+    console.log(users);
+    console.log(passwords);
+
+    if (!username || users.indexOf( username ) == -1){
+        return handleLoginError()
+    }
+    if (!password || passwords.indexOf( password ) == -1){
+        return handleLoginError()
+    }
+
+    return window.location.href = "camppass.html";
+}
+
+function handleAdminLogin(event){
+    console.log("LOGIN")
+    event.preventDefault();
+    let errorMsg = document.querySelector("#error-msg");
+    errorMsg.innerHTML = "";
+    let username = document.querySelector("#usernameInput").value;
+    let password = document.querySelector("#passwordInput").value;
+    console.log(username,password);
+
+    let data = mockInfo();
+    
+    let personnelData = data.personnel;
+    let users = personnelData.map((p)=> {
+        return p.username
+    });
+    let passwords = personnelData.map((p)=> {
+        return p.password
+    });
+
+    console.log(users);
+    console.log(passwords);
+
+    if (!username || users.indexOf( username ) == -1){
+        return handleLoginError()
+    }
+    if (!password || passwords.indexOf( password ) == -1){
+        return handleLoginError()
+    }
+
+    return 
+}
+
+function handleLoginError(){
+    let errorMsg = document.querySelector("#error-msg");
+    errorMsg.innerHTML = "Username or Password is incorrect."
+}
