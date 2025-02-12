7f71b7c89313091e128e1590dfae5fe2f1b25c59
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2025-02-12 12:05:24 +0800

    udpated html and data

--- data.js
+++ data.js
@@ -1,10 +1,72 @@
 function mockInfo() {
     let data = {
         "personnel": [
-            { "firstName": "John", "lastName": "Doe", "username":"johndoe", "password":"password123","unit":"32CSSB", "company":"maintenance", "designation":"oc" },
-            { "firstName": "Jack", "lastName": "Joe", "username":"jackjoe", "password":"password123","unit":"32CSSB", "company":"maintenance", "designation":"pc" },
-            { "firstName": "Peter", "lastName": "Jackson", "username":"peterjackson", "password":"password123","unit":"32CSSB", "company":"maintenance", "designation":"ps" },
-            { "firstName": "Wes", "lastName": "Anderson", "username":"wesanderson", "password":"password123","unit":"32CSSB", "company":"maintenance", "designation":"man" }
+            { 
+                "firstName": "Hawk", 
+                "lastName": "Eye", 
+                "username": "hawkeye", 
+                "password": "password123", 
+                "unit": "3DIV", 
+                "company": "none", 
+                "platoon": "none",
+                "designation": "admin",
+                "permissions": "full-control" 
+            },
+            { 
+                "firstName": "John", 
+                "lastName": "Doe", 
+                "username": "johndoe", 
+                "password": "password123", 
+                "unit": "32CSSB", 
+                "company": "security", 
+                "platoon": "hq", 
+                "designation": "oc",
+                "permissions": "company-control" 
+            },
+            { 
+                "firstName": "James", 
+                "lastName": "Poe", 
+                "username": "jamespoe", 
+                "password": "password123", 
+                "unit": "32CSSB", 
+                "company": "security", 
+                "platoon": "hq", 
+                "designation": "2ic",
+                "permissions": "company-control" 
+            },
+            { 
+                "firstName": "Jack", 
+                "lastName": "Joe", 
+                "username": "jackjoe", 
+                "password": "password123", 
+                "unit": "32CSSB", 
+                "company": "security",  
+                "platoon": "2", 
+                "designation": "pc",
+                "permissions": "platoon-control" 
+            },
+            { 
+                "firstName": "Peter", 
+                "lastName": "Jackson", 
+                "username": "peterjackson", 
+                "password": "password123", 
+                "unit": "32CSSB", 
+                "company": "security",  
+                "platoon": "2", 
+                "designation": "ps",
+                "permissions": "platoon-control" 
+            },
+            { 
+                "firstName": "Wes", 
+                "lastName": "Anderson", 
+                "username": "wesanderson", 
+                "password": "password123", 
+                "unit": "32CSSB", 
+                "company": "security",  
+                "platoon": "2", 
+                "designation": "man",
+                "permissions": "user-only"  
+            }
         ]
     }
 
